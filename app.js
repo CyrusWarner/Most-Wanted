869cc3f9d9cb1bac0be8d271859fe0076f178a5a
BA_CalderonMorales <tbcm640@gmail.com>
2021-06-07 13:42:04 -0500

    Added search by descendants function

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 'use strict';
 
 let filteredPeople;
+let personObject = {
+    id: "",
+    firstName: "",
+    lastName: "",
+    gender: "",
+    dob: "",
+    height: "",
+    weight: "",
+    eyeColor: "",
+    occupation: "",
+    parents: [],
+    currentSpouse: ""
+};;
 
 function searchByName() {
     // Grabbing the values from our nameForm form and inputs.
     let firstNameInput = document.forms['nameForm']['fname'].value;
     let lastNameInput = document.forms['nameForm']['lname'].value;
-    
-    correctCasing(firstNameInput, lastNameInput); // Correct casing check
 
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     filteredPeople = people.filter(function (person) {
-        if (person.firstName === firstNameInput && person.lastName === lastNameInput) {
-            return true;
+        if (person.firstName.toLowerCase() === firstNameInput.toLowerCase() 
+        && person.lastName.toLowerCase() === lastNameInput.toLowerCase()) {
+            document.getElementById("mostWanted").innerHTML += `<tr>
+            <td>${person.id}</td>
+            <td>${person.firstName}</td>
+            <td>${person.lastName}</td>
+            <td>${person.gender}</td>
+            <td>${person.dob}</td>
+            <td>${person.height}</td>
+            <td>${person.weight}</td>
+            <td>${person.eyeColor}</td>
+            <td>${person.occupation}</td>
+            <td>${person.parents}</td>
+            <td>${person.currentSpouse}</td>
+            </tr>`
         }
-        return false;
     });
-
-
 
     // Rather than console logging, you need to append the filteredPeople to a table.
     if (filteredPeople.length > 0) {
@@ -25,23 +46,8 @@ function searchByName() {
     } else {
         console.log('Sorry, looks like there is no one with that name.');
     }
-}
 
-//Correct casing
-function correctCasing(firstName, lastName) {
-    people.map(function (el) {
-        if (el.firstName.toLowerCase() === firstName.toLowerCase()
-            && el.lastName.toLowerCase() === lastName.toLowerCase()) {
-            document.getElementById("validOrNot").innerHTML = "<h1>Valid</h1>"
-            
-            //Build the table if the correct name is inputted into the form.
-            buildTable(el); // Populate table
-        }
-        else {
-            console.log("Invalid first or last name");
-        }
-
-    })
+    return personObject;
 }
 
 //Builds the table based off the specific person
@@ -63,13 +69,15 @@ function buildTable(){
 })
 }
 
+
+//Search by specific criteria
 let criteria;
 
 function searchBy(){
     let genderInput = document.forms['nameForm']['gender'].value;
     let eyeColorInput = document.forms.nameForm.eyeColor.value;   
     let flag = false;
-    filteredGender = people.filter(function (criteria) {
+    let filteredGender = people.filter(function (criteria) {
         if(genderInput.length > 0){
             flag = true;
         }
@@ -94,4 +102,52 @@ function searchBy(){
         if(filteredGender > 0){
             console.log(filteredGender);
         }
+}
+
+
+//Search by decendants
+function searchByChildren(){
+    // let currentPerson = searchByName();
+    let theFirstName = document.forms['nameForm']['fname'].value;
+    let theLastName = document.forms['nameForm']['lname'].value;
+    
+    people.filter(function (person) {
+        if (person.firstName.toLowerCase() === theFirstName.toLowerCase() 
+        && person.lastName.toLowerCase() === theLastName.toLowerCase()) {
+            personObject =  
+            {
+                "id": person.id,
+                "firstName": person.firstName,
+                "lastName": person.lastName,
+                "gender": person.gender,
+                "dob": person.dob,
+                "height": person.height,
+                "weight": person.weight,
+                "eyeColor": person.eyeColor,
+                "occupation": person.occupation,
+                "parents": person.parents,
+                "currentSpouse": person.currentSpouse
+            };
+        }
+    });
+
+    people.map(function(el) { 
+        if (theLastName === el.lastName 
+            && el.parents[0] === personObject.id
+            || el.parents[1] === personObject.id){
+            document.getElementById("mostWanted").innerHTML += `<tr>
+            <td>${el.id}</td>
+            <td>${el.firstName}</td>
+            <td>${el.lastName}</td>
+            <td>${el.gender}</td>
+            <td>${el.dob}</td>
+            <td>${el.height}</td>
+            <td>${el.weight}</td>
+            <td>${el.eyeColor}</td>
+            <td>${el.occupation}</td>
+            <td>${el.parents}</td>
+            <td>${el.currentSpouse}</td>
+            </tr>`
+        }
+    });
 }
